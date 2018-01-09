95106389785bd6ff4173e5ae39ec69415fbc8b7e
Andy Little <kwikius@yahoo.com>
2018-01-09 13:36:21 +0000

    Work on implementing derivative as outlined in https://github.com/msteinbeck/tinyspline/issues/99 Howvere don't know how to get implement "control points corresponding to a knot in a spline"

--- examples/c/interpolation_derivative.c
+++ examples/c/interpolation_derivative.c
@@ -22,6 +22,56 @@ tsBSpline spline;
 tsBSpline deriv;
 GLUnurbsObj *theNurb;
 
+/*
+
+A simple solution is to reduce to multiplicity of an internal knot by 1 
+if it is equals to order (deg + 1) and if the two corresponding control points 
+(https://github.com/msteinbeck/tinyspline/blob/master/library/tinyspline.h#L240) 
+are equals
+ (you can use: https://github.com/msteinbeck/tinyspline/blob/master/library/tinyspline.h#L889 
+to check if two control points are "equals"). 
+When reducing the multiplicity of such a knot we have to merge the two control points by 
+shifting the control point array to left so that the second control point corresponding 
+to our knot is replaced by the next one and so forth:
+
+// control points 
+[p0, p1, p2, *p3, p4, p5, p6] // p2 and p3 need to be merged
+[p0, p1, p2, p4, p5, p6] // fixed
+
+Similarly, we can shift the knot array:
+
+[0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4, 5, 6, 7, 8, 9, 9, 9, 9] // 4 needs to be merged
+[0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 5, 6, 7, 8, 9, 9, 9, 9] // fixed
+
+This step can be done before performing the actual derivation.
+
+*/
+
+// find multiplicity of a knot
+static tsError get_knot_multiplicity(tsBSpline* spline,size_t knot_idx, size_t * result)
+{
+     assert(spline != NULL);
+     assert(result != NULL);
+     assert(knot_idx < spline->n_knots);
+
+     tsDeBoorNet net;
+     const tsError e = ts_bspline_evaluate(spline,spline->knots[knot_idx],&net);
+     if ( e == TS_SUCCESS){
+        *result = net.s;
+     }
+     return e;
+}
+
+/*
+   get the 2 ? control points corresponding to a knot at index knot_idx in the spline
+*/
+static tsError get_corresponding_control_points(
+      tsBSpline* spline,size_t knot_idx,
+      tsReal const * result_ctrlpa, tsReal const * result_ctrlpb)
+{
+    return TS_DIM_ZERO;// error for now
+}
+
 /********************************************************
 *                                                       *
 * Modify these lines for experimenting.                 *
@@ -47,7 +97,29 @@ void setup()
 	points[14] = 0;
 
 	ts_bspline_interpolate_cubic(points, 5, 3, &spline);
+
+   const size_t n_knots = spline.n_knots;
+   const size_t deg = spline.deg;
+   const size_t dim = spline.dim;
+   for ( size_t knot_idx = 0U; knot_idx < n_knots; ++knot_idx){
+      size_t multiplicity = 0U;
+      tsError e = get_knot_multiplicity(&spline,knot_idx,&multiplicity);
+      assert ( e == TS_SUCCESS);
+      if ( multiplicity == (deg + 1)){
+         tsReal const * ctrl_pointsa;
+         tsReal const * ctrl_pointsb;
+         tsError e = get_corresponding_control_points(&spline,knot_idx,ctrl_pointsa,ctrl_pointsb);
+         assert ( e == TS_SUCCESS);
+         // compare the points
+         const tsReal min_useful_distance = (tsReal)1.e-6;// ? for now
+         if (ts_ctrlp_dist2(ctrl_pointsa,ctrl_pointsb, dim) < min_useful_distance){
+            printf("problem knot at %u, value %f:\n",(unsigned int)knot_idx,spline.knots[knot_idx]) ;
+         }
+      }
+   }
+
 //########################################
+   //N.B Will assert here until this is sorted
    assert (ts_bspline_derive(&spline,&deriv) == TS_SUCCESS);
 //##########################################
 	ts_bspline_print(&spline);
