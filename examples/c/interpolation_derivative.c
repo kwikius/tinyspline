9523a2337875cb225254a9c98c718375cedd2ba1
Andy Little <kwikius@yahoo.com>
2018-01-10 00:11:40 +0000

    tidy up a little. In the OpenGL view, show the original spline and cleaed spline in different positions

--- examples/c/interpolation_derivative.c
+++ examples/c/interpolation_derivative.c
@@ -18,8 +18,11 @@
 #include <debugging.h>
 #include <assert.h>
 
-tsBSpline spline;
+
+tsBSpline spline_orig;
+tsBSpline spline_copy;
 tsBSpline deriv;
+
 GLUnurbsObj *theNurb;
 
 /*
@@ -45,15 +48,6 @@ Similarly, we can shift the knot array:
 
 This step can be done before performing the actual derivation.
 
-static tsError remove_knot(tsBSpline* spline_in,size_t knot_index,tsBSpline* spline_out )
-{
-   make a knot vector of size one less than spline and copy all < knot_index and all > knot_index to it
-   assert( spline_in != spline_out);
-
-   
-
-}
-
 */
 
 /********************************************************
@@ -63,8 +57,8 @@ static tsError remove_knot(tsBSpline* spline_in,size_t knot_index,tsBSpline* spl
 ********************************************************/
 void setup()
 {
-
    printf ("interpolated spline derivative test with control point cleaning\n\n");
+
 	tsReal points[15];
 	points[0] = 1;
 	points[1] = -1;
@@ -82,27 +76,27 @@ void setup()
 	points[13] = 5;
 	points[14] = 0;
 
-	ts_bspline_interpolate_cubic(points, 5, 3, &spline);
+	ts_bspline_interpolate_cubic(points, 5, 3, &spline_orig);
 
    printf("original spline:\n");
-   ts_bspline_print(&spline);
+   ts_bspline_print(&spline_orig);
 
    printf("\nchecking spline for derivative issues\n");
 
-  // const size_t n_knots = spline.n_knots;
-   const size_t order = spline.order;
-   const size_t dim = spline.dim;
+   // Let us work on a copy of the spline
+   assert(ts_bspline_copy(&spline_orig,&spline_copy)== TS_SUCCESS);
+   const size_t order = spline_copy.order;
+   const size_t dim = spline_copy.dim;
    const tsReal min_useful_distance = (tsReal)1.e-6;// ? for now
 
    size_t knot_idx = 0U;
-   
-   // n.b that number of  knots and control points will change dynamically in the loop
-   while(knot_idx < spline.n_knots){
+   // n.b that number of knots and control points in the spline_copy may change dynamically in the loop
+   while(knot_idx < spline_copy.n_knots){
 
       // count the multiplicity of the knot
       size_t multiplicity = 1U;
-      const tsReal cur_knot_value = spline.knots[knot_idx];
-      while ( (knot_idx < (spline.n_knots-1)) && ( spline.knots[knot_idx +1] == cur_knot_value) ){
+      const tsReal cur_knot_value = spline_copy.knots[knot_idx];
+      while ( (knot_idx < (spline_copy.n_knots-1)) && ( spline_copy.knots[knot_idx +1] == cur_knot_value) ){
          ++knot_idx;
          ++multiplicity;
       }
@@ -110,28 +104,28 @@ void setup()
          const size_t idxa = (knot_idx - multiplicity) * dim;
          const size_t idxb = idxa + dim;
          // get and compare the control points
-         const tsReal* ctrl_pointsa = &spline.ctrlp[idxa];
-         const tsReal* ctrl_pointsb = &spline.ctrlp[idxb];
+         const tsReal* ctrl_pointsa = &spline_copy.ctrlp[idxa];
+         const tsReal* ctrl_pointsb = &spline_copy.ctrlp[idxb];
      
          if (ts_ctrlp_dist2(ctrl_pointsa,ctrl_pointsb, dim) < min_useful_distance){
 
             printf("found problem knot at index %u, removing...\n\n",(unsigned int)knot_idx) ;
 
-            // create and init a temporary spline for the mods
+            // create and init a temporary spline_copy for the mods
             tsBSpline mod_spline;
             {
-               mod_spline.deg = spline.deg;
-               mod_spline.order = spline.order;
-               mod_spline.dim = spline.dim;
-               mod_spline.n_ctrlp = spline.n_ctrlp-1U;
-               mod_spline.n_knots = spline.n_knots-1U;
+               mod_spline.deg = spline_copy.deg;
+               mod_spline.order = spline_copy.order;
+               mod_spline.dim = spline_copy.dim;
+               mod_spline.n_ctrlp = spline_copy.n_ctrlp-1U;
+               mod_spline.n_knots = spline_copy.n_knots-1U;
                const size_t n_raw_bytes_for_real_array = sizeof(tsReal) *  (mod_spline.n_knots + mod_spline.n_ctrlp * mod_spline.dim);
                mod_spline.ctrlp = (tsReal*) malloc(n_raw_bytes_for_real_array);
                // TODO check malloc result
                mod_spline.knots = mod_spline.ctrlp + mod_spline.n_ctrlp * mod_spline.dim;
             }
-            // copy control points prior to and including first control point coresponding to current knot
-            const tsReal* ctrlp_src = spline.ctrlp;
+            // copy control points prior to and including first control point corresponding to current knot
+            const tsReal* ctrlp_src = spline_copy.ctrlp;
             tsReal* ctrlp_dest = mod_spline.ctrlp;
 
             size_t reals_copied = 0U;
@@ -140,7 +134,7 @@ void setup()
                *ctrlp_dest++ = *ctrlp_src++;
                 ++reals_copied;
             }
-            // skip copying 2nd control point coresponding to current knot
+            // skip copying 2nd control point corresponding to current knot
             ctrlp_src +=  mod_spline.dim;
             // copy the rest of the control points over
             while ( reals_copied < (mod_spline.n_ctrlp * mod_spline.dim) ){
@@ -148,7 +142,7 @@ void setup()
                 ++reals_copied;
             }
             // copy all knots before current
-            const tsReal* knots_src = spline.knots;
+            const tsReal* knots_src = spline_copy.knots;
             tsReal* knots_dest = mod_spline.knots;
             size_t idx = 0U;
             while ( idx < knot_idx){
@@ -160,12 +154,12 @@ void setup()
                knots_dest[idx] = knots_src[idx + 1];
                ++idx;
             } 
-            // clear the original spline and replace the contents with the new one  
-            ts_bspline_free(&spline);
-            ts_bspline_move(&mod_spline,&spline);
+            // clear the original spline_copy and replace the contents with the new one  
+            ts_bspline_free(&spline_copy);
+            ts_bspline_move(&mod_spline,&spline_copy);
             
             printf("... spline after problem knot removed:\n");
-            ts_bspline_print(&spline);
+            ts_bspline_print(&spline_copy);
          }
       }else{
          // nothing to do
@@ -175,62 +169,78 @@ void setup()
 
    printf("spline cleaned:\n\n");
 
-   assert (ts_bspline_derive(&spline,&deriv) == TS_SUCCESS);
+   assert (ts_bspline_derive(&spline_copy,&deriv) == TS_SUCCESS);
 
    printf("show spline derivative:\n");
-	//ts_bspline_print(&spline);
    ts_bspline_print(&deriv);
+
 }
 
 void tear_down()
 {
-	ts_bspline_free(&spline);
+	ts_bspline_free(&spline_copy);
+   ts_bspline_free(&spline_orig);
+   ts_bspline_free(&deriv);
 }
 
-void display(void)
+// function to show the two splines in varying colours, where each colour is a array of dim 3
+static void draw_spline(tsBSpline * spline,GLfloat* spline_colour,GLfloat* ctrlp_colour,GLfloat* eval_colour)
 {
-	tsDeBoorNet net;
-	size_t i;
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	
-	/* draw spline */
-	glColor3f(1.0, 1.0, 1.0);
+   tsDeBoorNet net;
 	glLineWidth(3);
+   glColor3f(spline_colour[0],spline_colour[1],spline_colour[2]);
 	gluBeginCurve(theNurb);
 		gluNurbsCurve(
 			theNurb, 
-			(GLint)spline.n_knots,
-			spline.knots, 
-			(GLint)spline.dim,
-			spline.ctrlp, 
-			(GLint)spline.order,
+			(GLint)spline->n_knots,
+			spline->knots, 
+			(GLint)spline->dim,
+			spline->ctrlp, 
+			(GLint)spline->order,
 			GL_MAP1_VERTEX_3
 		);
 	gluEndCurve(theNurb);
 
-	/* draw control points */
-	glColor3f(1.0, 0.0, 0.0);
+   glColor3f(ctrlp_colour[0],ctrlp_colour[1],ctrlp_colour[2]);
 	glPointSize(5.0);
 	glBegin(GL_POINTS);
-	  for (i = 0; i < spline.n_ctrlp; i++) 
-		 glVertex3fv(&spline.ctrlp[i * spline.dim]);
+     size_t i;
+	  for (i = 0; i < spline->n_ctrlp; i++) 
+		 glVertex3fv(&spline->ctrlp[i * spline->dim]);
 	glEnd();
-	
+ 
 	/* draw evaluation */
-	glColor3f(0.0, 0.0, 1.0);
+	glColor3f(eval_colour[0],eval_colour[1],eval_colour[2]);
 	glPointSize(5.0);
-	ts_bspline_evaluate(&spline, 0.5f, &net);
+	ts_bspline_evaluate(spline, 0.5f, &net);
 	glBegin(GL_POINTS);
 		glVertex3fv(net.result);
 	glEnd();
-	ts_deboornet_free(&net);
+   ts_deboornet_free(&net);
+}
+
+static GLfloat red[]= { 1.0,0.0,0.0};
+static GLfloat green[]= { 0,1.0,0.0};
+static GLfloat blue[]= { 0.0,0.0,1.0};
+static GLfloat grey[]= { 0.5,0.5,0.5};
+static GLfloat yellow[]= { 1.0,1.0,0.0};
+static GLfloat orange[]= { 1.0,0.75,0.0};
+
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	
+   glTranslatef(-4.f, 1.5f, 0.0f);
+
+   draw_spline(&spline_orig,yellow,red,green);
+
+   glTranslatef(4.f, -1.5f, 0.0f);
+
+   draw_spline(&spline_copy,grey,orange,blue);
+
 	glutSwapBuffers();
 	glutPostRedisplay();
 }
-
-
-
 
 /********************************************************
 *                                                       *
